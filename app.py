9610bd322c44bb9763d757b3a1e497898d867021
JureStabuc <jure.stabuc@gmail.com>
2017-01-28 11:40:22 +0000

    Created a detail page about each attack

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 def get_csv():
     csv_path ="./static/top50weu.csv"
-    csv_file = open(csv_path, "r")
+    #adding encoding because of issues with it. http://stackoverflow.com/questions/12468179/unicodedecodeerror-utf8-codec-cant-decode-byte-0x9c
+    csv_file = open(csv_path, "r", encoding="latin-1")
     #parsed and returned as a list of dictionaries
     csv_obj = csv.DictReader(csv_file)
     csv_list = list(csv_obj)
@@ -16,6 +17,14 @@ def index():
     template ="index.html"
     object_list = get_csv()
     return render_template(template, object_list=object_list)
+
+@app.route("/<row_id>/")
+def detail(row_id):
+    template = "detail.html"
+    object_list = get_csv()
+    for row in object_list:
+        if row["id"] == row_id:
+            return render_template(template, object=row)
 
 if __name__ == "__main__":
     #Fire up the Flask test server
